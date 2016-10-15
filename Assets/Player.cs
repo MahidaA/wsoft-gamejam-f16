b36f79fe8dfb1f0081961ac5ad478ab3a8fe8f7e
MahidaA <amil.mahida@gmail.com>
2016-10-14 22:33:59 -0400

    Fixed some player movement bugs

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-    int speed;
+    public int speed;
+    public int jumpSpeed;
     Rigidbody2D rb;
+    bool faceRight;
+    bool grounded;
 
 	// Use this for initialization
 	void Start () {
         speed = 3;
+        jumpSpeed = 5;
         rb = GetComponent<Rigidbody2D>();
+        faceRight = true;
+        grounded = true;
 	}
 	
 	// Update is called once per frame
@@ -17,16 +23,43 @@ public class Player : MonoBehaviour {
 	    if (Input.GetKey("right"))
         {
             transform.Translate(speed * Vector2.right * Time.deltaTime);
+            faceRight = true;
         }
 
-        if (Input.GetKeyDown("up"))
+        if (Input.GetKeyDown("up") && grounded)
         {
-            rb.velocity = Vector2.up * 5;
+            rb.velocity = Vector2.up * jumpSpeed;
+            grounded = false;
         }
 
         if (Input.GetKey("left"))
         {
             transform.Translate(speed * Vector2.left * Time.deltaTime);
+            faceRight = false;
         }
 	}
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.gameObject.tag == "pcol")
+        {
+            grounded = true;
+        }
+
+        if (coll.gameObject.tag == "Ledge")
+        {
+            //if (coll.gameObject.transform.position.x > transform.position.x && 
+            //    Input.GetKey("Right") && !grounded)
+            //{
+            transform.Translate(Vector2.up * 10 * Time.deltaTime);
+            transform.Translate(Vector2.right * 5 * Time.deltaTime);
+            //}
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D coll) {
+        if (coll.gameObject.tag == "pcol")
+        {
+            grounded = false;
+        }
+    }
 }
