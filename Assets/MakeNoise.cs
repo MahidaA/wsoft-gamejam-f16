11824ffacba895f9572e55da9195105a8682a111
adamcbrown <adamcbrown1997@gmail.com>
2016-10-15 17:54:17 -0400

    Added distraction making components

---
+++ Assets/MakeNoise.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Distraction))]
+public class MakeNoise : MonoBehaviour {
+
+	bool state; //true if someone is within the trigger
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("e") && state)
+		{
+			GetComponent<Distraction>().distract();
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			state = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			state = false;
+		}
+	}
+}
