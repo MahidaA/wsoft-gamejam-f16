1d5002859b32417fd217185ce90d657f9bc519e6
MahidaA <amil.mahida@gmail.com>
2016-10-14 20:55:23 -0400

    Added simple player script with left-right movement and jump

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    int speed;
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        speed = 3;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey("right"))
+        {
+            transform.Translate(speed * Vector2.right * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown("up"))
+        {
+            rb.velocity = Vector2.up * 5;
+        }
+
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(speed * Vector2.left * Time.deltaTime);
+        }
+	}
+}
