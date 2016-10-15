e72a0715b8775a16b06547829a5f0f46585575d5
MahidaA <amil.mahida@gmail.com>
2016-10-15 11:29:21 -0400

    Improved grounding detection
    It now works with any object with a box collider and tagged as
    "Platform"

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -13,21 +13,37 @@ public class Player : MonoBehaviour {
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
         faceRight = true;
-        grounded = true;
+        grounded = false;
 	}
+
+    bool isGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, GetComponent<Collider2D>().bounds.extents.y + 0.01f);
+        if (hit.collider == null)
+            return false;
+
+        if (hit.transform.tag == "Platform" || hit.transform.tag == "pcol")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
 	    if (Input.GetKey("right"))
         {
             transform.Translate(speed * Vector2.right * Time.deltaTime);
             faceRight = true;
         }
 
-        if (Input.GetKeyDown("up") && grounded)
+        if (Input.GetKeyDown("space") && isGrounded())
         {
             rb.velocity = Vector2.up * jumpSpeed;
-            grounded = false;
         }
 
         if (Input.GetKey("left"))
@@ -35,6 +51,7 @@ public class Player : MonoBehaviour {
             transform.Translate(speed * Vector2.left * Time.deltaTime);
             faceRight = false;
         }
+
 	}
 
     void OnTriggerStay2D(Collider2D other)
@@ -42,32 +59,19 @@ public class Player : MonoBehaviour {
         if (other.gameObject.tag == "Ledge")
         {
             if (other.gameObject.transform.position.x >= transform.position.x &&
-            Input.GetKey("right") && !grounded)
+            Input.GetKey("right") && !isGrounded())
             {
                 transform.Translate(Vector2.up * 2 * Time.deltaTime);
                 transform.Translate(Vector2.right * Time.deltaTime);
             }
 
             if (other.gameObject.transform.position.x <= transform.position.x &&
-            Input.GetKey("left") && !grounded)
+            Input.GetKey("left") && !isGrounded())
             {
                 transform.Translate(Vector2.up * 2 * Time.deltaTime);
                 transform.Translate(Vector2.left * Time.deltaTime);
             }
         }
     }
-
-    void OnCollisionEnter2D(Collision2D coll) {
-        if (coll.gameObject.tag == "pcol")
-        {
-            grounded = true;
-        }
-    }
-
-    void OnCollisionExit2D(Collision2D coll) {
-        if (coll.gameObject.tag == "pcol")
-        {
-            grounded = false;
-        }
-    }
+ 
 }
