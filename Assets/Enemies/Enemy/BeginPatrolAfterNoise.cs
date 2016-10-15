9bebc573ac5a21c1f5633809241f00aa167f9874
adamcbrown <adamcbrown1997@gmail.com>
2016-10-15 17:59:42 -0400

    Patrol after noise script

---
+++ Assets/Enemies/Enemy/BeginPatrolAfterNoise.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(GuardThenPatrol))]
+public class BeginPatrolAfterNoise : MonoBehaviour {
+	
+	void Update () {
+		if(GetComponent<Enemy>().isEngaging){
+			GetComponent<GuardThenPatrol>().guarding=false;
+		}
+	}
+}
