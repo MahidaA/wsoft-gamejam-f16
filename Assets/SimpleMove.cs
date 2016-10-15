cfca3e5746d8a6e871d6430729fc563b90ab4ffe
acerio98 <a.c.riopelle@me.com>
2016-10-14 21:23:07 -0400

    Built a basic setup in my scene
    There is a tree that moves left and right and a background… that kind
    of works? Still doesn’t do that much, but it’s just the background
    anyway; not hard stuff.

---
+++ Assets/SimpleMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMove : MonoBehaviour {
+
+	public float mySpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.D)){
+			gameObject.transform.position = new Vector3(gameObject.transform.position.x - mySpeed, gameObject.transform.position.y, gameObject.transform.position.z);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			gameObject.transform.position = new Vector3(gameObject.transform.position.x + mySpeed, gameObject.transform.position.y, gameObject.transform.position.z);
+		}
+	}
+}
