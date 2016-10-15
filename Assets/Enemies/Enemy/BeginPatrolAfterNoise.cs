b5feb78f211e1286f31612b9b6becc9401f41276
adamcbrown <adamcbrown1997@gmail.com>
2016-10-15 19:16:55 -0400

    Added guard then patrol so it automatically guards at the start

--- Assets/Enemies/Enemy/BeginPatrolAfterNoise.cs
+++ Assets/Enemies/Enemy/BeginPatrolAfterNoise.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 [RequireComponent (typeof(GuardThenPatrol))]
 public class BeginPatrolAfterNoise : MonoBehaviour {
+
+	void Awake(){
+		GetComponent<GuardThenPatrol>().guarding=true;
+	}
 	
 	void Update () {
 		if(GetComponent<Enemy>().isEngaging()){
