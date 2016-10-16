ee34937493d1e146f8bb4b91f31e2571a1a20d17
acerio98 <a.c.riopelle@me.com>
2016-10-16 00:10:39 -0400

    Updated 2.5D scene again
    Now it has a ledge that you can climb up and fall off of. All of it is
    based upon a more robust “levels” system: one ledge brings you up 1
    level; the ladder here brings you up 2 levels, etc.

---
+++ Assets/LedgeScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LedgeScript : MonoBehaviour {
+
+	public WalkScript playerScript;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(!playerScript.isJumping && !playerScript.isClimbingLedge){
+			playerScript.fall();
+		}
+	}
+
+	void OnTriggerStay(Collider other) {
+		if((other.transform.position.x < gameObject.transform.position.x && playerScript.isRight) ||
+		   (other.transform.position.x > gameObject.transform.position.x && !playerScript.isRight)){
+			playerScript.inClimbZone = true;
+			Debug.Log("YES");
+		}
+		else{
+			playerScript.inClimbZone = false;
+			Debug.Log("NO");
+		}
+	}
+	void OnTriggerExit(Collider other){
+		playerScript.inClimbZone = false;
+	}
+}
