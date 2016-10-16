9d646bc7d8deff2dfa74444def6f089ab8a25579
adamcbrown <adamcbrown1997@gmail.com>
2016-10-16 16:52:42 -0400

    Main menu and credits

---
+++ Assets/Credits.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+
+	void OnGUI(){
+		if(GUI.Button(new Rect(Screen.width/5, Screen.height*8/10, Screen.width*3/5, Screen.height/6), "Back to Menu")){
+			SceneManager.LoadScene("SceneMenu");
+		}
+	}
+}
