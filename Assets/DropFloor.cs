8fca553aeb52ef228d155a5ac2458e34d3ac23cd
adamcbrown <adamcbrown1997@gmail.com>
2016-10-16 13:05:50 -0400

    Improved ladder movement

---
+++ Assets/DropFloor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropFloor : MonoBehaviour {
+
+	void Update(){
+		GetComponent<Collider2D>().isTrigger=Input.GetKey(KeyCode.DownArrow);
+	}
+}
